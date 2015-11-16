4f1bb4df071b4625ca51e801785199d156c7382c
huzr <sacrize@poczta.fm>
2015-11-16 17:22:55 +0100

    .

--- WebSite1/login_page.aspx.cs
+++ WebSite1/login_page.aspx.cs
@@ -15,6 +15,6 @@ public partial class login_page : System.Web.UI.Page
     {
         Session["USER_ID"] = login_lab.Text;
         Session["PERMISSION"] = 0;
-        Response.Redirect("~/MojeZgloszenia.aspx", false);
+        Response.Redirect("~/rap.aspx", false);
     }
 }
