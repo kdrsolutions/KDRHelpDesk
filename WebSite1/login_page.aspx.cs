d2a383f30a4a33c0fa2491d692d3ea4a7bfa292f
huzr <sacrize@poczta.fm>
2015-11-22 19:03:37 +0100

    .

--- WebSite1/login_page.aspx.cs
+++ WebSite1/login_page.aspx.cs
@@ -23,16 +23,22 @@ public partial class login_page : System.Web.UI.Page
         /* ########################## */
         //sqlstring = "UPDATE [Uzytkownicy] SET HashHaslo = HashBytes('SHA1', '" + password_lab.Text + "') WHERE Login = '" + login_lab.Text + "';";
 
-        if (Authentication(sqlstring, login_lab.Text, password_lab.Text))
-        //if (Authentication(sqlstring, false))
+        System.Data.SqlClient.SqlConnection con = new System.Data.SqlClient.SqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings["HelpDesk1ConnectionString"].ConnectionString);
+        System.Data.SqlClient.SqlCommand comm = new System.Data.SqlClient.SqlCommand(sqlstring, con);
+        comm.Parameters.AddWithValue("userID", login_lab.Text);
+        comm.Parameters.AddWithValue("haslo", password_lab.Text);
+        System.Data.SqlClient.SqlDataReader reader;
+        con.Open();
+        reader = comm.ExecuteReader();
+
+        if (reader.Read())
         {
             Session["USER_ID"] = login_lab.Text;
-
             // CHECK ADMIN PERMISSIONs
-            Session["PERMISSION_ADMIN"] = Authentication(sqlstring);
+            Session["PERMISSION_ADMIN"] = reader.GetValue(3);
 
             // CHECK SPEC PERMISSIONs
-            Session["PERMISSION_SPEC"] = Authentication(sqlstring);
+            Session["PERMISSION_SPEC"] = reader.GetValue(2);
 
             Response.Redirect("~/rap.aspx", false);
         }
@@ -40,26 +46,5 @@ public partial class login_page : System.Web.UI.Page
         {
             wrong_password.Visible = true;
         }
-    }
-    protected static Boolean Authentication(string sqlstring, string userID, string haslo)
-    //protected static Boolean Authentication(string sqlstring, bool func_type)
-    // func_type = false - > logowanie
-    // func_type = true - > uprawnienia
-    {
-        System.Data.SqlClient.SqlConnection con = new System.Data.SqlClient.SqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings["HelpDesk1ConnectionString"].ConnectionString);
-        System.Data.SqlClient.SqlCommand comm = new System.Data.SqlClient.SqlCommand(sqlstring, con);
-        comm.Parameters.AddWithValue("userID", userID);
-        comm.Parameters.AddWithValue("haslo", haslo);
-        System.Data.SqlClient.SqlDataReader reader;
-        con.Open();
-        reader = comm.ExecuteReader();
-        if (reader.Read())
-            if (func_type)
-                return (bool)reader.GetValue(0);
-            else
-                return true;
-        else
-            
-            return false;
     }
 }
