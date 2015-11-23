cb3e2a03b0cdf5eccc6af05aa911ef7bca903f0f
Hinarus <fabian.schulke@gmail.com>
2015-11-23 17:33:10 +0100

    ssss

---
+++ WebSite1/Controls/test.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Configuration;
+using System.Data;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+
+public partial class Controls_test : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    protected void Button2_Click(object sender, EventArgs e)
+    {
+        Mailing.Send("marcinsiekierski97@o2.pl", "Witaj kolego", "Jestes kolega");
+    }
+}
