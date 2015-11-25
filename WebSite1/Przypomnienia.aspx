cb08fe7f0340bdf8a254451756c9f8050f3d37e8
huzr <sacrize@poczta.fm>
2015-11-25 11:27:57 +0100

    ...

--- WebSite1/Przypomnienia.aspx
+++ WebSite1/Przypomnienia.aspx
@@ -3,31 +3,8 @@
 <asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
-    <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="True" 
-         DataSourceID="SqlDataSource1">
-       
-    </asp:GridView>
 
-
-
-    <asp:SqlDataSource ID="SqlDataSource1" runat="server" 
-   
-        ConnectionString="<%$ ConnectionStrings:HelpDesk1ConnectionString %>" 
-        SelectCommand="SELECT *from Przypomnienia">
-        
-       
-        
-        </asp:SqlDataSource>
-      
-      
-    <asp:SqlDataSource ID="SqlDataSource2" runat="server" 
-   
-        ConnectionString="<%$ ConnectionStrings:HelpDesk1ConnectionString %>" 
-        SelectCommand="SELECT *from ZgloszeniaStatusy">
-        
-       
-        
-        </asp:SqlDataSource>
-
+    <ul ID="ListBox1" runat="server">
+    </ul>
 </asp:Content>
 
--- WebSite1/Przypomnienia.aspx.cs
+++ WebSite1/Przypomnienia.aspx.cs
@@ -10,11 +10,34 @@ using System.Web.UI.HtmlControls;
 using System.Web.UI.WebControls;
 using System.Web.UI.WebControls.WebParts;
 using System.Xml.Linq;
+using System.Data.SqlClient;
 
 public partial class Przypomnienia : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+        string sqlstring;
+        sqlstring = "SELECT Opis FROM [Przypomnienia] WHERE DataNastWywol <= GETDATE();";
 
+        using (var conn = new SqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings["HelpDesk1ConnectionString"].ConnectionString))
+        using (var cmd = conn.CreateCommand())
+        {
+            conn.Open();
+            cmd.CommandText = sqlstring;
+            var reader = cmd.ExecuteReader();
+            using (reader)
+            {
+                while (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        HtmlGenericControl li = new HtmlGenericControl("li");
+                        li.InnerText = (string)reader.GetValue(0);
+                        ListBox1.Controls.Add(li);
+                    }
+                    reader.NextResult();
+                }
+            }
+        }
     }
 }
